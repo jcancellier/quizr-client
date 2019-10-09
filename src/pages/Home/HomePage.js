90c1ee2648cde81a63b270f4ef66dcd918315838
Joshua Cancellier <jcancellier@csub.edu>
2019-10-09 02:33:22 -0700

    added join (login) button

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
-import { TextField } from '@material-ui/core';
-import { HomePageContainer } from './style';
+import { 
+  TextField, 
+  Button, 
+  Typography
+} from '@material-ui/core';
+import { Person } from '@material-ui/icons';
+import {
+  HomePageContainer,
+  AuthFormTextContainer,
+  AuthFormContainer,
+  Divider
+} from './style';
 
 class HomePage extends Component {
   state = {
@@ -14,13 +24,34 @@ class HomePage extends Component {
   render() {
     return (
       <HomePageContainer>
-        <TextField
-          id='username-input'
-          label='Username'
-          value={this.state.usernameInputText}
-          onChange={e => this.handleUsernameInputTextChange(e)}
-          variant='filled'
-        />
+        <AuthFormContainer>
+          <AuthFormTextContainer>
+            <TextField
+              id='username-input'
+              label='Username'
+              value={this.state.usernameInputText}
+              onChange={e => this.handleUsernameInputTextChange(e)}
+              variant='outlined'
+            />
+            {/* Use the components below if password will be utilized */}
+            {/* <Divider/>
+            <TextField
+              id='password-input'
+              label='Password'
+              value={this.state.usernameInputText}
+              onChange={e => this.handleUsernameInputTextChange(e)}
+              variant='filled'
+              type="password"
+            /> */}
+          </AuthFormTextContainer>
+          <Button
+              variant='contained'
+              color='primary'
+              endIcon={ <Person /> }
+          >
+            Join
+          </Button>
+        </AuthFormContainer>
       </HomePageContainer>
     )
   }
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -3,6 +3,27 @@ import styled from 'styled-components';
 export const HomePageContainer = styled.div`
   display: flex;
   flex: 1;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
+`;
+
+export const AuthFormTextContainer = styled.div`
+  display: flex;
+  padding-bottom: 1rem;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+export const AuthFormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  /* align-items: center; */
+`;
+
+export const Divider = styled.div`
+  width: 0.5rem;
 `;
