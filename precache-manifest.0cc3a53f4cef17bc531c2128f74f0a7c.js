1c9767e3495a218f7d593fdd4ed4b4996b541bdc
Joshua Cancellier <jcancellier@csub.edu>
2019-10-15 01:18:46 -0700

    Updates

--- precache-manifest.0cc3a53f4cef17bc531c2128f74f0a7c.js
+++ precache-manifest.0cc3a53f4cef17bc531c2128f74f0a7c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ece7d459c56820578eeb14c7fc601617",
+    "revision": "be73f6d40314427338760fd00e069f9b",
     "url": "/quizr-client/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/quizr-client/static/js/2.c89222a8.chunk.js"
   },
   {
-    "revision": "6be5edaaf849de91b399",
-    "url": "/quizr-client/static/js/main.7be3a367.chunk.js"
+    "revision": "74e43871ac2cedd195bb",
+    "url": "/quizr-client/static/js/main.dc4bf980.chunk.js"
   },
   {
     "revision": "4ee812cf33a45924a1d1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/quizr-client/precache-manifest.6e22d92bccbef8476ab737d315558755.js"
+  "/quizr-client/precache-manifest.0cc3a53f4cef17bc531c2128f74f0a7c.js"
 );
 
 self.addEventListener('message', (event) => {
