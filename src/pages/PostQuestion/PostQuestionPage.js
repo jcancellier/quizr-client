d06659bc71461a1d015757b8d016efcf80ef7578
Garrett Miller <milleregem@gmail.com>
2019-12-04 12:06:46 -0800

    Pre/Post Question

--- src/pages/PostQuestion/PostQuestionPage.js
+++ src/pages/PostQuestion/PostQuestionPage.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
+import { AppHeader } from '../../global/components';
 import { connect } from 'react-redux';
+import { Content, Header, PostQuestionPageContainer, Footer } from './style';
+import { Typography } from '@material-ui/core';
+import CheckIcon from '@material-ui/icons/Check';
 
-class PostQuestion extends Component {
+class PostQuestionPageComponent extends Component {
   render() {
     return (
-      <div>
-        <p>After Question</p>
-      </div>
+      <PostQuestionPageContainer>
+        <AppHeader buttons={false} />
+        <Header>
+          <CheckIcon style={{fontSize: 'calc(15px + 8vw)'}}/>
+          <Typography variant="h4" style={{ fontSize: 'calc(10px + 1.5vw)' }}>
+          Correct!
+          </Typography>
+          <Typography variant="h4" style={{ fontSize: 'calc(10px + .7vw)' }}>
+          +1 points
+          </Typography>
+        </Header>
+
+        <Content>
+        </Content>
+      
+        <Footer>     
+        </Footer>
+      </PostQuestionPageContainer>
     );
   }
 }
 
-const PostQuestionPage = connect()(PostQuestion);
+const PostQuestionPage = connect()(PostQuestionPageComponent);
 
 export { PostQuestionPage };
--- src/pages/Prequestion/PrequestionPage.js
+++ src/pages/Prequestion/PrequestionPage.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
+import { AppHeader } from '../../global/components';
 import { connect } from 'react-redux';
+import { Content, Header, PrequestionPageContainer, Footer } from './style';
+import { Typography } from '@material-ui/core';
+import { Timer } from '../../global/components';
 
-class Prequestion extends Component {
+
+class PrequestionPageComponent extends Component {
   render() {
     return (
-      <div>
-        <p>Preparing Question</p>
-      </div>
+      <PrequestionPageContainer>
+        <AppHeader buttons={false} />
+        <Header>
+          <Typography variant="h4" style={{ fontSize: 'calc(10px + .8vw)' }}>
+          Question 1 of 5
+          </Typography>
+          <Typography variant="h4" style={{ fontSize: 'calc(10px + 2vw)' }}>
+          Who is the current president of CSUB?          
+          </Typography>
+        </Header>
+
+        <Content>
+          <div style={{display: 'flex', flex: 1, justifyContent: 'flex-start', flexdirection: 'column'}}>
+            <Timer time={0} />
+          </div>
+        </Content>
+          
+        <Footer>     
+        </Footer>
+      </PrequestionPageContainer>
     );
   }
 }
 
-const PrequestionPage = connect()(Prequestion);
+const PrequestionPage = connect()(PrequestionPageComponent);
 
 export { PrequestionPage };
