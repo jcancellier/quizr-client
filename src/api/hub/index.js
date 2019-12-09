5f2f7c5a6149527ce0fbcd6bd68ef0710a370e9b
Joshua Cancellier <jcancellier@csub.edu>
2019-12-08 19:03:45 -0800

    actually finished

--- src/api/hub/index.js
+++ src/api/hub/index.js
@@ -4,4 +4,4 @@ export const quizrHubDevelopmentUrl = "https://localhost:44328/quizrhub";
 export const quizrHubProductionUrl = "https://quizr-api.herokuapp.com/quizrhub";
 export const quizrHubDevelopmentMacUrl = "https://localhost:5001/quizrhub";
 
-export const quizrHubUrl = quizrHubDevelopmentMacUrl;
+export const quizrHubUrl = quizrHubProductionUrl;
